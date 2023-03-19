e3977fc48af9671c1de994c95122c9fc0dd0dbf1
Vasudev Menon <ee20947@bristol.ac.uk>
2023-03-19 15:13:18 +0000

    Added previous contents of studsy files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+
+``
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBzf2p2M8KhydM5T4Xrb2DQapG7UL3XwP8",
+    authDomain: "studsyai.firebaseapp.com",
+    projectId: "studsyai",
+    storageBucket: "studsyai.appspot.com",
+    messagingSenderId: "354106067055",
+    appId: "1:354106067055:web:5c5c7fa650c4c5002be59a",
+    measurementId: "G-7P241SQRQH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
+
+
+// dropzone component
+// drag drop file component
+function DragDropFile() {
+    // drag state
+    const [dragActive, setDragActive] = React.useState(false);
+    // ref
+    const inputRef = React.useRef(null);
+
+    // handle drag events
+    const handleDrag = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.type === "dragenter" || e.type === "dragover") {
+            setDragActive(true);
+        } else if (e.type === "dragleave") {
+            setDragActive(false);
+        }
+    };
+
+    // triggers when file is dropped
+    const handleDrop = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        setDragActive(false);
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            // handleFiles(e.dataTransfer.files);
+        }
+    };
+
+    // triggers when file is selected with click
+    const handleChange = function (e) {
+        e.preventDefault();
+        if (e.target.files && e.target.files[0]) {
+            var file = e.target.files[0];
+            var storageRef = storage.ref();
+            var fileRef = storageRef.child(file.name);
+            fileRef.put(file).then(function (snapshot) {
+                console.log("Uploaded a file!");
+            });
+        }
+    };
+
+
+    // triggers the input when the button is clicked
+    const onButtonClick = () => {
+        inputRef.current.click();
+    };
+
+    return (
+        <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+            <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+            <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : ""}>
+                <div>
+                    <p>Drag and drop your file here or</p>
+                    <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+                </div>
+            </label>
+            {dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div>}
+        </form>
+    );
+};
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <DragDropFile />
+            </header>
+        </div>
+    );
 }
 
 export default App;
+
+
